6f993b846c8d7b3172cb5c2662525fa4cdca18ef
Marshall Munoz <Marshall.Munoz@fbitn.com>
2018-02-12 10:29:49 -0600

    finished the program into testing

---
+++ src/Charecters/MainController.java
@@ -0,0 +1,7 @@
+package Charecters;
+
+public class MainController {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/MyProgram.java
+++ src/MyProgram.java
@@ -1,5 +1,7 @@
 import Charecters.Creature;
+import Charecters.Enemy;
 import Charecters.Player;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.util.Scanner;
 
@@ -28,8 +30,86 @@ public class MyProgram{
 
         scene(player, "a mansion");
         if(!player.isDead()){
-            scence(player, "a rock");
+            scene(player, "a rock");
         }
     }
-    public String attack(Creature one, Creature Two)
+    public String attack(Creature one, Creature two){
+        int a = one.attack(two);
+        return one.getName() + " hit " + two.getName() + " for " + a + " damage.";
+    }
+
+    public void battle(Player one, Creature two){
+        System.out.println(one);
+        System.out.println(two);
+
+        while(true){
+            Scanner readChoice = new Scanner(System.in);
+            System.out.print("\nWhat do you want to do(Attack, Run, Status, Use Potion): ");
+            String userChoice = readChoice.nextLine();
+            while (true) {
+                if (!userChoice.equalsIgnoreCase("Status") && !userChoice.equalsIgnoreCase("Run") && !userChoice.equalsIgnoreCase("Attack") && !userChoice.equalsIgnoreCase("Use Potion")){
+                    System.out.println("Choose a valid choice");
+                    readChoice = new Scanner(System.in);
+                    System.out.print("\nWhat do you want to do (Attack, Run, Status, Use Potion): ");
+                    userChoice = readChoice.nextLine();
+            }else{
+                    break;
+                }
+        }
+        if(userChoice.equalsIgnoreCase("Status")){
+            System.out.println(one.status());
+
+            continue;
+        }
+
+        if(userChoice.equalsIgnoreCase("Run")){
+            int run =(int)(Math.random() * 100 + 1);
+            if(run >= 50){
+                System.out.println("You successfully run.");
+                break;
+            }else{
+                System.out.println("Your enemy wont let you run.");
+
+            }
+
+        }else if(userChoice.equalsIgnoreCase("Attack")){
+            System.out.println(attack(one, two));
+            System.out.println(two.status());
+
+            }
+            if(!two.isDead()){
+                System.out.println(attack(two, one));
+                System.out.println(one.status());
+                if(one.isDead()){
+                    System.out.println("You died!");
+                    break;
+                }
+            }else{
+                System.out.println("You killed " + two.getName() + "\n");
+                System.out.println("you gained " + one.gainXp(two) + " exp");
+                if(one.checkXp()){
+                    System.out.println("You leveled up, your health is restored!");
+                    System.out.println("You have " + one.getXp() + " exp");
+                }else{
+                    System.out.println("You have " + one.getXp() + " exp");
+                }
+                System.out.println(one + "\n");
+                break;
+            }
+        }
+    }
+    public void scene(Player one, String description){
+        System.out.println(one.getName() + " arrives at " + description);
+
+        int x =(int)(Math.random()* 3 + 1);
+
+        for(int i =0; i < x; i++){
+            if(one.isDead()){
+                break;
+            }
+            Enemy randEnemy = new Enemy(one.getLevel());
+            System.out.println("\nYou encounter " + randEnemy.getName() + " the " + randEnemy.getRole());
+            battle(one,randEnemy);
+        }
+    }
 }
